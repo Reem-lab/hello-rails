dd29e247f3d1dacd1ccab22e160bebe424f2bab5
Reem <janinareem@gmail.com>
2022-04-26 16:48:15 +0200

    Create new rails project and print hello rails

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'pages/hello'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: 'pages#hello'
+end
+
+
